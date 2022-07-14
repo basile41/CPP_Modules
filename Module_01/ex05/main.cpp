4bc7e9ce682401d172e4020d70ab3332d923f718
Basile REGNEAU <bregneau@student.42.fr>
2022-07-14 21:57:08 +0200

    Module_01 ex05 ok

--- Module_01/ex05/main.cpp
+++ Module_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 18:36:20 by bregneau          #+#    #+#             */
-/*   Updated: 2022/07/14 20:35:19 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/07/14 21:54:38 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int main(int argc, char **argv)
 {
 	Harl	harl;
 
-	if (argc >= 2)
+	if (argc == 2)
 		harl.complain(argv[1]);
 }
