8caadc0b80de952b6d421caf393cde71a1da5182
Basile REGNEAU <bregneau@student.42.fr>
2022-07-14 19:00:57 +0200

    Module_01 ex05 WIP

---
+++ Module_01/ex05/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 18:37:04 by bregneau          #+#    #+#             */
+/*   Updated: 2022/07/14 18:41:33 by bregneau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void	complain( std::string level );
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+};
+
+#endif
