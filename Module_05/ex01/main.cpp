f88a10cf35436f738670b531e28e009b87a7bbd5
Basile REGNEAU <bregneau@student.42.fr>
2022-07-25 20:32:16 +0200

    Module_05 ex01 modif main

--- Module_05/ex01/main.cpp
+++ Module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:36:44 by bregneau          #+#    #+#             */
-/*   Updated: 2022/07/25 20:22:30 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/07/25 20:29:46 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,24 @@ int main()
 		std::cout << form00 << std::endl;
 		Bureaucrat	bob("Bob", 43);
 		std::cout << "Hello I am " << bob << "." << std::endl;
-		bob.signForm(form00);
-		bob.upgrade();
-		bob.signForm(form00);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+		try
+		{
+			bob.signForm(form00);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 
-	try
-	{
-		Form form00("form00", 42);
-		std::cout << form00 << std::endl;
-		Bureaucrat	bob("Bob", 43);
-		std::cout << "Hello I am " << bob << "." << std::endl;
-		bob.upgrade();
-		bob.signForm(form00);
+		try
+		{
+			bob.upgrade();
+			bob.signForm(form00);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
 	catch(const std::exception& e)
 	{
