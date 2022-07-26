4b0f8b4f2c8c44b9ea7ba94047de10a0081d5306
Basile REGNEAU <bregneau@student.42.fr>
2022-07-26 14:40:42 +0200

    Module_05 ex01 fix

--- Module_05/ex01/main.cpp
+++ Module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:36:44 by bregneau          #+#    #+#             */
-/*   Updated: 2022/07/25 20:29:46 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/07/26 14:39:38 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ int main()
 		{
 			std::cerr << e.what() << std::endl;
 		}
+
+		std::cout << form00 << std::endl;
 	}
 	catch(const std::exception& e)
 	{
