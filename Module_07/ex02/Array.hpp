509aa4f72d77bdf118175704e6e9b4206193f450
Basile REGNEAU <bregneau@student.42.fr>
2022-08-01 17:56:13 +0200

    Module_07 ex02 ok

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 17:53:54 by bregneau          #+#    #+#             */
-/*   Updated: 2022/07/31 18:00:08 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/08/01 17:52:08 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,65 @@ class Array
 
 	public:
 
-		Array();
-		Array( unsigned int n );
-		Array( Array const & src )
+		Array() : _size(0), _tab(NULL)
+		{}
+		Array( unsigned int n ) : _size(n), _tab(new T[n])
 		{
-			*this
+			for (unsigned int i = 0; i < _size ; i++)
+				_tab[i] = 0;
 		}
-		~Array();
+		Array( Array<T> const & src ) : _tab(NULL)
+		{
+			*this = src;
+		}
 
-		Array &		operator=( Array const & rhs );
+		~Array()
+		{
+			delete[] _tab;
+		}
+
+		Array<T> &		operator=( Array<T> const & rhs )
+		{
+			delete[] _tab;
+			_size = rhs._size;
+			_tab = new T[rhs._size];
+			for (unsigned int i = 0; i < _size ; i++)
+			{
+				_tab[i] = rhs._tab[i];
+			}
+			return *this;
+		}
+		T &			operator[](unsigned int pos)
+		{
+			if (pos >= _size)
+				throw std::exception();
+			return _tab[pos];
+		}
+		T const &	operator[](unsigned int pos) const
+		{
+			if (pos >= _size)
+				throw std::exception();
+			return _tab[pos];
+		}
+
+		unsigned int	size() const
+		{
+			return _size;
+		}
 
 	private:
+		unsigned int	_size;
+		T *				_tab;
 
 };
 
-std::ostream &			operator<<( std::ostream & o, Array const & i );
+template<typename T>
+std::ostream &	operator<<( std::ostream & o, Array<T> const & array )
+{
+	o << "{";
+	for (unsigned int i = 0; i < array.size(); i++)
+		o << array[i] << (i != array.size() - 1 ? ", ": "}");
+	return o;
+}
 
 #endif /* *********************************************************** ARRAY_H */
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -6,14 +6,49 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 14:43:08 by bregneau          #+#    #+#             */
-/*   Updated: 2022/07/31 17:39:33 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/08/01 17:33:33 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Array.hpp"
 
 int main()
 {
-	
+	Array<int> int_array(5);
+	int_array[0] = 1;
+	int_array[1] = 2;
+	int_array[2] = 3;
+	int_array[3] = 4;
+	int_array[4] = 5;
+
+	Array<int> const  const_int_array(int_array);
+	int_array[1] = -545;
+	int_array[3] = 42;
+		
+	std::cout << int_array << std::endl;
+	std::cout << const_int_array << std::endl;
+
+	try
+	{
+		int_array[5];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	Array<float> test;
+	try
+	{
+		test[0];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	int_array = const_int_array;
+	std::cout << int_array << std::endl;
 	return 0;
 }
